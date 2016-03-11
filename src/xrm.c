53013ada2c494f4913c87e6553be8f89df3ba5de
Ingo Bürk <ingo.buerk@tngtech.com>
2016-03-11 10:13:32 +0100

    Remove first draft again

--- src/xrm.c
+++ src/xrm.c
@@ -75,12 +75,8 @@ void xcb_xrm_context_free(xcb_xrm_context_t *ctx) {
 int xcb_xrm_resource_get(xcb_xrm_context_t *ctx, const char *res_name, const char *res_class,
                          const char **res_type, xcb_xrm_resource_t **_resource) {
     xcb_xrm_resource_t *resource;
-    xcb_xrm_entry_t *curr;
     xcb_xrm_entry_t *entry_name = NULL;
     xcb_xrm_entry_t *entry_class = NULL;
-    xcb_xrm_hay_entry_t *cur_db_entry, *next_db_entry;
-    xcb_xrm_component_t *cur_res_name_component;
-    TAILQ_HEAD(entries_head, xcb_xrm_hay_entry_t) entries_head = TAILQ_HEAD_INITIALIZER(entries_head);
     int result = 0;
 
     if (ctx->resources == NULL || TAILQ_EMPTY(&(ctx->entries))) {
@@ -107,92 +103,13 @@ int xcb_xrm_resource_get(xcb_xrm_context_t *ctx, const char *res_name, const cha
         goto done;
     }
 
-    /* First, we set up a wrapper list of our database so we can store some
-     * additional information for each entry and also filter entries out. */
-    TAILQ_FOREACH(curr, &(ctx->entries), entries) {
-        xcb_xrm_hay_entry_t *new = scalloc(1, sizeof(xcb_xrm_hay_entry_t));
-        new->entry = curr;
-        new->current_component = TAILQ_FIRST(&(curr->components));
+    // TODO XXX Algorithm :(
 
-        TAILQ_INSERT_TAIL(&entries_head, new, entries);
-    }
-
-    // TODO XXX This will currently eat our actual database and fail with the second request. Fix this.
-    // TODO XXX We currently ignore res_class. Implement handling it.
-    // TODO XXX The last component must be treated case insensitive.
-    // TODO XXX This currently doesn't implement precedence at all.
-    // https://tronche.com/gui/x/xlib/resource-manager/matching-rules.html
-    cur_res_name_component = TAILQ_FIRST(&(entry_name->components));
-    while (cur_res_name_component != NULL) {
-        cur_db_entry = TAILQ_FIRST(&entries_head);
-
-        while (cur_db_entry != NULL) {
-            next_db_entry = NULL;
-
-            /* If the queried resource is longer than this entry, eliminate it. */
-            if (cur_db_entry->current_component == NULL) {
-                goto eliminate_entry;
-            }
-
-            if (cur_db_entry->current_component->type == CT_NORMAL) {
-                /* We are comparing against a non-wildcard component, so just compare names. */
-                if (strcmp(cur_db_entry->current_component->name, cur_res_name_component->name) == 0) {
-                    /* The name matches, so we don't filter this entry out. */
-                    goto keep_entry;
-                } else {
-                    /* No match. Better luck next time! */
-                    goto eliminate_entry;
-                }
-            } else if (cur_db_entry->current_component->type == CT_WILDCARD_SINGLE) {
-                /* We'll happily accept the placeholder and move on. */
-                goto keep_entry;
-            } else {
-                // TODO XXX Handle wildcards
-            }
-
-eliminate_entry:
-            next_db_entry = TAILQ_NEXT(cur_db_entry, entries);
-            TAILQ_REMOVE(&entries_head, cur_db_entry, entries);
-            FREE(cur_db_entry);
-keep_entry:
-            if (next_db_entry == NULL && cur_db_entry != NULL) {
-                next_db_entry = TAILQ_NEXT(cur_db_entry, entries);
-            }
-
-            if (cur_db_entry != NULL) {
-                cur_db_entry->current_component = TAILQ_NEXT(cur_db_entry->current_component, components);
-            }
-
-            cur_db_entry = next_db_entry;
-        }
-
-        cur_res_name_component = TAILQ_NEXT(cur_res_name_component, components);
-    }
-
-    /* If we filtered everything out, there's no result. */
-    if (TAILQ_EMPTY(&entries_head)) {
-        result = -1;
-        goto done;
-    }
-
-    // TODO XXX Implement precedence here
-    resource->value = sstrdup(TAILQ_FIRST(&entries_head)->entry->value);
-    resource->size = strlen(resource->value);
+    // TODO XXX Fill
+    resource->value = sstrdup("");
+    resource->size = 0;
 
 done:
-    while (!TAILQ_EMPTY(&entries_head)) {
-        cur_db_entry = TAILQ_FIRST(&entries_head);
-        TAILQ_REMOVE(&entries_head, cur_db_entry, entries);
-        FREE(cur_db_entry);
-    }
-
-    if (entry_name != NULL) {
-        xcb_xrm_entry_free(entry_name);
-    }
-    if (entry_class != NULL) {
-        xcb_xrm_entry_free(entry_class);
-    }
-
     return result;
 }
 
