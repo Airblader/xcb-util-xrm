33e2cac36cf300296042726ba7275b5a0559a9d7
Ingo Bürk <ingo.buerk@tngtech.com>
2016-03-17 11:28:30 -0400

    Implement line continuation.
    This patch introduces line continuation via '\' + newline as described in
    the specification.
    
    fixes #10

--- tests/test.c
+++ tests/test.c
@@ -302,6 +302,7 @@ static int test_get_resource(void) {
     err |= check_get_resource(ctx, "First.second: 1", "First.third", "first.second", "1");
     err |= check_get_resource(ctx, "First.second.third: 1", "First.third.third", "first.second.fourth", "1");
     err |= check_get_resource(ctx, "First*third*fifth: 1", "First.second.third.fourth.third.fifth", "", "1");
+    err |= check_get_resource(ctx, "First: x\\\ny", "First", "", "xy");
     /* Matching among multiple entries */
     err |= check_get_resource(ctx,
             "First: 1\n"
